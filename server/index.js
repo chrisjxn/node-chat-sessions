5c37f92aaef1393ee054ac6895c78894de39ae40
chrisjxn <usjacksons@gmail.com>
2017-10-21 09:49:10 -0600

    commit changes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mc = require( `${__dirname}/controllers/messages_controller` );
+const mc = require(`${__dirname}/controllers/messages_controller`);
+const session = require('express-session');
+
+const filter = require('./middlewares/filter')
+const createInitialSession = require('./middlewares/session');
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../public/build` ) );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../public/build`));
+app.use(session({
+    secret: 'the m00se w@lks b@ckw@rd 1n the r@1n',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 100000000 }
+}));
+
+app.use(createInitialSession);
+
+app.use((req, res, next) => {
+    if(req.method === 'POST' || req.method === 'PUT') {
+        filter(req, res, next);
+    }
+    next();
+})
+
 
 const messagesBaseUrl = "/api/messages";
-app.post( messagesBaseUrl, mc.create );
-app.get( messagesBaseUrl, mc.read );
-app.put( `${messagesBaseUrl}`, mc.update );
-app.delete( `${messagesBaseUrl}`, mc.delete );
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}`, mc.update);
+app.delete(`${messagesBaseUrl}`, mc.delete);
+app.get(`${messagesBaseUrl}/history`, mc.history);
 
 const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+app.listen(port, () => { console.log(`Server listening on port ${port}.`); });
